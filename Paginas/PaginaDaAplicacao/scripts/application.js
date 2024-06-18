38b5f8ace52041fcaf722cf5c764e9cfe23742c3
Juliano Santos <julianosantosdev@outlook.com>
2024-06-18 12:44:07 -0300

    fix: menu closes click outside aside

--- Paginas/PaginaDaAplicacao/scripts/application.js
+++ Paginas/PaginaDaAplicacao/scripts/application.js
@@ -1,13 +1,14 @@
 const leftMenu = () => {
+  const body = document.querySelector("body");
   const aside = document.getElementById("leftMenu");
   const hamburguerMenu = document.getElementById("hamburguerMenu");
   hamburguerMenu.addEventListener("click", (event) => {
     aside.classList.toggle("menu__hidden");
   });
 
-  aside.addEventListener("click", (event) => {
-    if (event.currentTarget != event.target) {
-      menuSection.classList.toggle("menu__hidden");
+  body.addEventListener("click", (event) => {
+    if (event.clientX > 250 && !aside.classList.contains("menu__hidden")) {
+      aside.classList.toggle("menu__hidden");
     }
   });
 };
